d334a7cf5eaa4711e6b3f4ed27d54af61bada5a3
Tr1p1e <130800295+Tr1p1e@users.noreply.github.com>
2023-12-01 21:37:44 -0500

    Add files via upload

--- UBRFID/myapp/forms.py
+++ UBRFID/myapp/forms.py
@@ -1,11 +1,17 @@
-from django import forms 
+
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 class SignupForm(UserCreationForm):
+    ubid_number = forms.CharField(max_length=30, required=True, help_text='Required. Enter your UBID Number.')
+    phonenumber = forms.CharField(max_length=15, required=True, help_text='Required. Enter your phone number.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your first name.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Required. Enter your last name.')
+
     class Meta:
-        model = User 
-        fields = ['username', 'password1', 'password2']
+        model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'password1', 'password2', 'ubid_number', 'phonenumber']
 
 class LoginForm(forms.Form):
     username = forms.CharField()
--- UBRFID/myapp/models.py
+++ UBRFID/myapp/models.py
@@ -1,3 +1,12 @@
+# models.py in myapp
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ubid_number = models.CharField(max_length=30, blank=True, null=True)
+    phonenumber = models.CharField(max_length=15, blank=True, null=True)
+    # Add any other additional fields you need
+
+    def __str__(self):
+        return self.username
